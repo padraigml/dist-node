79a78905318581b9fbf7846312328228a05cfdd4
padraigml <bb20070@bristol.ac.uk>
2021-12-10 15:01:47 +0000

    Update node.go

--- node.go
+++ node.go
@@ -102,7 +102,7 @@ func main() {
 
 	go Listen(pAddr, &Node{Close: false})
 
-	brokerAddr := "127.0.0.1:8000"
+	brokerAddr := "192.168.0.13:8000"
 	client, err := rpc.Dial("tcp", brokerAddr)
 	fmt.Println("here")
 	if err != nil {
